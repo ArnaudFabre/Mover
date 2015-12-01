8807303305ad0dbd3ec94959d9b8a903a9c094d0
Arnaud Fabre <arnaud.fabre@gmail.com>
2015-12-01 09:58:51 +0100

    Add translate and scale with center, getTranslate* becomes scale agnostic

--- mover.js
+++ mover.js
@@ -1,14 +1,14 @@
 var Mover = function () {
-    
+
     // Fix Javascript rounding errors
     var rounder = 1e5;
     function round(x) {
         return Math.round(x*rounder)/rounder;
     }
-    
+
     // Default is identity
     var data = [1, 0, 0, 0, 1, 0, 0, 0, 1];
-    
+
     // Dump function for debug
     function dump() {
         console.log("------------");
@@ -17,12 +17,12 @@ var Mover = function () {
         console.log(this.data[6] + " " + this.data[7] + " " + this.data[8]);
         return this;
     }
-    
+
     // Internal Multiplication of 2 Matrix
     function multiply(m1, m2) {
         var i, j, k;
         var m3 = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-        
+
         for(i=0; i<3; i++) {
             for(j=0; j<3; j++) {
                 for(k=0; k<3; k++) {
@@ -32,28 +32,42 @@ var Mover = function () {
         }
         return m3;
     }
-    
+
     function zero() {
         this.data = [0, 0, 0, 0, 0, 0, 0, 0, 0];
         return this;
     }
-    
+
     function identity() {
         this.data = [1, 0, 0, 0, 1, 0, 0, 0, 1];
         return this;
     }
-    
+
     function translation(x, y) {
         this.data = [1, 0, x, 0, 1, y, 0, 0, 1];
         return this;
     }
 
-    function translate(x, y) {
+    function translate(x, y, cx, cy) {
+        var center = false;
+        if(cx || cy)
+        {
+            cx = cx || 0;
+            cy = cy || 0;
+            center = true;
+        }
         var translation = [1, 0, x, 0, 1, y, 0, 0, 1];
+
+        if(center) {
+            this.translate(cx, cy);
+        }
         this.data = multiply(this.data, translation);
+        if(center) {
+            this.translate(-cx, -cy);
+        }
         return this;
     }
-    
+
     function rotation(angle) {
         var rad = angle*Math.PI/180;
         var cos = round(Math.cos(rad));
@@ -61,7 +75,7 @@ var Mover = function () {
         this.data = [cos, -sin, 0, sin, cos, 0, 0, 0, 1];
         return this;
     }
-    
+
     function rotate(angle, x, y) {
         var center = false;
         if(x || y)
@@ -84,7 +98,7 @@ var Mover = function () {
         }
         return this;
     }
-    
+
     function scaling(s) {
         if(s === 0) {
             s = 1;
@@ -92,34 +106,50 @@ var Mover = function () {
         this.data = [1, 0, 0, 0, 1, 0, 0, 0, 1/s];
         return this;
     }
-    
-    function scale(s) {
+
+    function scale(s, x, y) {
+        var center = false;
+        if(x || y)
+        {
+            x = x || 0;
+            y = y || 0;
+            center = true;
+        }
         if(s === 0) {
             s = 1;
         }
         var scaling = [1, 0, 0, 0, 1, 0, 0, 0, 1/s];
+
+        if(center) {
+            this.translate(x, y);
+        }
         this.data = multiply(this.data, scaling);
+        if(center) {
+            this.translate(-x, -y);
+        }
         return this;
     }
-    
+
     function apply(m, n) {
         if(typeof m == 'number') {
             m = [m, n];
         }
-        
+
         var e = 1;
         if(m.length == 3) {
             e = m[2];
         }
-        
+
         var x = round(m[0]*this.data[0] + m[1]*this.data[1] + e*this.data[2]);
         var y = round(m[0]*this.data[3] + m[1]*this.data[4] + e*this.data[5]);
         var w = round(m[0]*this.data[6] + m[1]*this.data[7] + e*this.data[8]);
-        
-        if(w !== 0)
+
+        if(w !== 0) {
             return [x/w, y/w];
-        else
+        }
+        else {
             return [x, y];
+        }
     }
 
     function getRotation() {
@@ -140,30 +170,15 @@ var Mover = function () {
     }
 
     function getTranslation() {
-        if(this.data[8] === 0) {
-            return [this.data[2], this.data[5]];
-        }
-        else {
-            return [this.data[2]/this.data[8], this.data[5]/this.data[8]];
-        }
+        return [this.data[2], this.data[5]];
     }
 
     function getTranslationX() {
-        if(this.data[8] === 0) {
-            return this.data[2];
-        }
-        else {
-            return this.data[2]/this.data[8];
-        }
+        return this.data[2];
     }
 
     function getTranslationY() {
-        if(this.data[8] === 0) {
-            return this.data[5];
-        }
-        else {
-            return this.data[5]/this.data[8];
-        }
+        return this.data[5];
     }
 
     function getScaling() {
@@ -179,33 +194,33 @@ var Mover = function () {
         return this;
     }
 
-  return {
-      dump:dump,
-      data:data,
-      
-      // Creation
-      zero:zero,
-      identity:identity,
-      rotation:rotation,
-      translation:translation,
-      scaling:scaling,
-      
-      // Accessors
-      getRotation:getRotation,
-      getTranslation:getTranslation,
-      getTranslationX:getTranslationX,
-      getTranslationY:getTranslationY,
-      getScaling:getScaling,
+    return {
+        dump:dump,
+        data:data,
 
-      // Apply transformation to a 2D point
-      apply:apply,
-      
-      // Combine
-      rotate:rotate,
-      translate:translate,
-      scale:scale,
+        // Creation
+        zero:zero,
+        identity:identity,
+        rotation:rotation,
+        translation:translation,
+        scaling:scaling,
 
-      // Compose Transforms
-      compose:compose
-  };
+        // Accessors
+        getRotation:getRotation,
+        getTranslation:getTranslation,
+        getTranslationX:getTranslationX,
+        getTranslationY:getTranslationY,
+        getScaling:getScaling,
+
+        // Apply transformation to a 2D point
+        apply:apply,
+
+        // Combine
+        rotate:rotate,
+        translate:translate,
+        scale:scale,
+
+        // Compose Transforms
+        compose:compose
+    };
 };
