1d39e9befa05f3f60d718db64b7869fd58e91f02
Arnaud Fabre <arnaud.fabre@gmail.com>
2015-12-03 10:13:35 +0100

    Update testing Playground

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Mover Framework allows to combine 2D transformations like
     
 It is based on homogeneous coordinates that allows to keep in a unique 3x3 matrix the combination of multiple plane transformations.
 
+A basic abstraction of boxes called Boxer is also provided.
+
 ## Examples
 Create a new mover instance:
 ```
@@ -51,4 +53,31 @@ You can also access translation, rotation and scaling factors.
 And compose different Movers into new ones :
 ```
     var moves = Mover().compose(myMove, otherMove, anotherOne, andLastMove);
+```
+
+You can export to css transform (note that transform-origin must be 0 0 to be compatible with Mover matrices) :
+```
+    image.style.transform = myMove.css();
+```
+
+## Boxer Example
+```
+    // Get Images
+    var red = document.getElementById('red');
+    var blue = document.getElementById('blue');
+
+    // Initialize Boxer objects
+    b1 = Boxer(red.width, red.height);
+    b2 = Boxer(blue.width, blue.height);
+    // Create a virtual box
+    b3 = Boxer().push(b1).push(b2);
+    
+    // Apply transforms
+    b1.translate(10, 10);
+    b2.rotate(90);
+    b3.translate(20, 0).rotate(-90);
+    
+    // Get final css
+    red.style.transform = b3.css(0);
+    blue.style.transform = b3.css(1);
 ```
--- test/testing.js
+++ test/testing.js
@@ -1,87 +1,34 @@
-// Copy/Paste from http://stackoverflow.com/questions/20256760/javascript-console-log-to-html 
-// Arun P Johny
-(function () {
-    var old = console.log;
-    var logger = document.getElementById('log');
+var red = document.getElementById('red');
+var blue = document.getElementById('blue');
+
+var l1, l2, l3, l4, p;
+
+window.onload = function () {
+    l1 = Boxer(red.width, red.height);
+    l2 = Boxer(blue.width, blue.height);
+    l3 = Boxer(red.width, red.height);
+    l3t = Boxer(red.width, red.height);
+    p = Boxer().push(l1).push(l2);
+};
+
+function update() {
+    // Test 3 combinations
+    var m1 = l1.mover();
+    var g = p.mover();
+    var m2 = l3.mover();
+    var t = l3t.mover();
+    red.style.transform = Mover().compose(t, g, m1, m2).css();
+    blue.style.transform = p.css(1);
     
-    if(logger) {
-    console.log = function (message) {
-        if (typeof message == 'object') {
-            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
-        } else {
-            logger.innerHTML += message + '<br />';
-        }
-    }
-    }
-    else {
-        alert("Can not found log");
-    }
+    /*
+    // Test Boxer of Boxers
+    red.style.transform = p.css(0);
+    blue.style.transform = p.css(1);
+    */
     
-})();
-
-// Testing
-console.log("Zero");
-Mover().zero().dump();
-
-console.log("Identity");
-Mover().identity().dump();
-
-console.log("Rotation 90");
-Mover().rotation(90).dump();
-
-console.log("Translation 1, 1");
-Mover().translation(1, 1).dump();
-
-console.log("Scaling 2");
-Mover().scaling(2).dump();
-
-console.log("============ Transforms ============");
-// Applying translation Mover
-console.log("Translate 1,1 by 1,1");
-var p = Mover().translation(1, 1).apply([1,1]);
-console.log(p + " == 2,2");
-
-console.log("Translate 1,1,0.5 by 1,1");
-// Applying translation Mover in homogeneous coordinates
-var p2 = Mover().translation(1, 1).apply([1,1,0.5]);
-console.log(p2 + " == 1.5,1.5,0.5 (3,3)");
-
-console.log("Chained transformation that should be translate 2,2");
-// Build a chained transformation
-var l1 = Mover().translate(1, 1).rotate(-90).translate(-1,1).rotate(90).dump();
-
-console.log("Apply to 1,0");
-// Apply chained transformation
-var p3 = l1.apply([1,0]);
-console.log(p3 + " == 3,2");
-
-// Add a scale
-console.log("Add a scale 2 (Homotethy => *2 )");
-l1.scale(2).dump();
-
-console.log("Chained transformation should be now translate by 2,2, then local homothety *2");
-// And apply it
-var p4 = l1.apply(1,0);
-console.log(p4 + " == 4,2");
-
-console.log("Test accessors");
-
-console.log(l1.getTranslation());
-console.log(l1.getTranslationX());
-console.log(l1.getTranslationY());
-console.log(l1.getRotation());
-console.log(l1.getScaling());
-
-var l2 = Mover().rotation(90);
-
-console.log(l2.getTranslation());
-console.log(l2.getTranslationX());
-console.log(l2.getTranslationY());
-console.log(l2.getRotation());
-console.log(l1.getScaling());
-
-console.log(Mover().rotate(-90).getRotation());
-console.log(Mover().rotate(-180).getRotation());
-console.log(Mover().rotate(180).getRotation());
-
-
+    /*
+    // Test Boxer
+    red.style.transform = l1.css();
+    blue.style.transform = l2.css();
+    */
+}
